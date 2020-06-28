0c92c0b298f17e9d854726fac7b7ca1a770f0427
Meir Keller <mrzkeller@gmail.com>
2020-06-28 12:50:25 +0300

    Updates

---
+++ precache-manifest.537fe8094390e64c57a57e33778a35cb.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f492f48fe36580244d386c7ea95558",
+    "url": "/whether-app/index.html"
+  },
+  {
+    "revision": "1fc013643eaa750a6154",
+    "url": "/whether-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1fc013643eaa750a6154",
+    "url": "/whether-app/static/js/2.a4cedef3.chunk.js"
+  },
+  {
+    "revision": "6e11391e26889c29e375",
+    "url": "/whether-app/static/js/main.015b3b50.chunk.js"
+  },
+  {
+    "revision": "4ec3a5053073fa7c175c",
+    "url": "/whether-app/static/js/runtime-main.741865e9.js"
+  },
+  {
+    "revision": "ddfc0a8a8d151837556663a9099be057",
+    "url": "/whether-app/static/media/1.ddfc0a8a.svg"
+  },
+  {
+    "revision": "b7d23a2e0616f4dcce1674ce98f68552",
+    "url": "/whether-app/static/media/11.b7d23a2e.svg"
+  },
+  {
+    "revision": "4ae37b87515044fb8d49b0ffdf9f462b",
+    "url": "/whether-app/static/media/12.4ae37b87.svg"
+  },
+  {
+    "revision": "a016fe25ddfb7014a7305d04457cd4ab",
+    "url": "/whether-app/static/media/13.a016fe25.svg"
+  },
+  {
+    "revision": "c05c7d6d6e1acae00d031f9d2fb350d3",
+    "url": "/whether-app/static/media/14.c05c7d6d.svg"
+  },
+  {
+    "revision": "cdd26e08feef3694489748dbc9d6b022",
+    "url": "/whether-app/static/media/15.cdd26e08.svg"
+  },
+  {
+    "revision": "2e40152ef69e6a3721e659ce8d5ffa8e",
+    "url": "/whether-app/static/media/16.2e40152e.svg"
+  },
+  {
+    "revision": "fcdcfb58c93eaf528a17c31ce7a1bd6e",
+    "url": "/whether-app/static/media/17.fcdcfb58.svg"
+  },
+  {
+    "revision": "2b6cdc0b98cea0a014b30fa3220814e5",
+    "url": "/whether-app/static/media/18.2b6cdc0b.svg"
+  },
+  {
+    "revision": "b235b7104c4455b19c303a2b37b72518",
+    "url": "/whether-app/static/media/19.b235b710.svg"
+  },
+  {
+    "revision": "4ac6ad3cd0eb0010ca63232dc9bee85e",
+    "url": "/whether-app/static/media/2.4ac6ad3c.svg"
+  },
+  {
+    "revision": "e0b917f8e0ce71df0235a252ba0a98f2",
+    "url": "/whether-app/static/media/20.e0b917f8.svg"
+  },
+  {
+    "revision": "a4f4c5dea548ab37bc6423e7c6c42a5e",
+    "url": "/whether-app/static/media/21.a4f4c5de.svg"
+  },
+  {
+    "revision": "9e93fb31850a50db874ebc6483b61c65",
+    "url": "/whether-app/static/media/22.9e93fb31.svg"
+  },
+  {
+    "revision": "3bcf692695c2a7b513a8ba7182eca329",
+    "url": "/whether-app/static/media/23.3bcf6926.svg"
+  },
+  {
+    "revision": "d2b0f64554eb9f0d6349be548102c57d",
+    "url": "/whether-app/static/media/24.d2b0f645.svg"
+  },
+  {
+    "revision": "586d62d14e3c8980810b3d28b80f1a1f",
+    "url": "/whether-app/static/media/25.586d62d1.svg"
+  },
+  {
+    "revision": "b97c7813f5b2299e710b1932f97251cc",
+    "url": "/whether-app/static/media/26.b97c7813.svg"
+  },
+  {
+    "revision": "22ff3c9dd357b468e15e296d0bf9df0e",
+    "url": "/whether-app/static/media/29.22ff3c9d.svg"
+  },
+  {
+    "revision": "5f9f7cbf5c36f227fd222fcc42f5caef",
+    "url": "/whether-app/static/media/3.5f9f7cbf.svg"
+  },
+  {
+    "revision": "53a0f033f7e4357ebe12bba73d98196e",
+    "url": "/whether-app/static/media/30.53a0f033.svg"
+  },
+  {
+    "revision": "03115ee9d5126748c9774ac1c2359b4b",
+    "url": "/whether-app/static/media/31.03115ee9.svg"
+  },
+  {
+    "revision": "7bd8a73ef9f5d90d9544e4f41d558502",
+    "url": "/whether-app/static/media/32.7bd8a73e.svg"
+  },
+  {
+    "revision": "5d33944e7634fdbeb19d67fc1762f944",
+    "url": "/whether-app/static/media/33.5d33944e.svg"
+  },
+  {
+    "revision": "e8935a1caaca37915d3a7b4269fe183d",
+    "url": "/whether-app/static/media/34.e8935a1c.svg"
+  },
+  {
+    "revision": "a8392907839fd73ee9d9a4546efeda1c",
+    "url": "/whether-app/static/media/35.a8392907.svg"
+  },
+  {
+    "revision": "fd0e1ab813cb7f70046860e1ab941fad",
+    "url": "/whether-app/static/media/36.fd0e1ab8.svg"
+  },
+  {
+    "revision": "aab97cbc1a48cdc17e10d3ebe4eb6322",
+    "url": "/whether-app/static/media/37.aab97cbc.svg"
+  },
+  {
+    "revision": "02ff2dc363f72d955c8da89de66ede61",
+    "url": "/whether-app/static/media/38.02ff2dc3.svg"
+  },
+  {
+    "revision": "353a3190b9897149036e215d1e6fdd73",
+    "url": "/whether-app/static/media/39.353a3190.svg"
+  },
+  {
+    "revision": "1b279cc96e9d38aac94a2cad110cb240",
+    "url": "/whether-app/static/media/4.1b279cc9.svg"
+  },
+  {
+    "revision": "ee7de7b4825588475dc253d9d3f947c0",
+    "url": "/whether-app/static/media/40.ee7de7b4.svg"
+  },
+  {
+    "revision": "d999b69f6f088b4d9ef594c2f3d270c8",
+    "url": "/whether-app/static/media/41.d999b69f.svg"
+  },
+  {
+    "revision": "cb78308ee20f688483f95aafb778af08",
+    "url": "/whether-app/static/media/42.cb78308e.svg"
+  },
+  {
+    "revision": "78728be9164a7cfa87f4c0b882bedc11",
+    "url": "/whether-app/static/media/43.78728be9.svg"
+  },
+  {
+    "revision": "fcb9619be6fbafc7e9684a9ced882fec",
+    "url": "/whether-app/static/media/44.fcb9619b.svg"
+  },
+  {
+    "revision": "882ca40de7fc45a55a4530d41c2a12c5",
+    "url": "/whether-app/static/media/5.882ca40d.svg"
+  },
+  {
+    "revision": "9ebeed5edfc69917e5c25b533f3527bd",
+    "url": "/whether-app/static/media/6.9ebeed5e.svg"
+  },
+  {
+    "revision": "9f1824cc2b3792f97a6c4b5da8255b4e",
+    "url": "/whether-app/static/media/7.9f1824cc.svg"
+  },
+  {
+    "revision": "cb8185b9490f76295a3094827df1f529",
+    "url": "/whether-app/static/media/8.cb8185b9.svg"
+  },
+  {
+    "revision": "3dcea32cc7e335367524a0dc49dba8aa",
+    "url": "/whether-app/static/media/close-circle-outline.3dcea32c.svg"
+  },
+  {
+    "revision": "3081492ef647f1b0a3d9c9a704ced354",
+    "url": "/whether-app/static/media/cloud-error.3081492e.svg"
+  },
+  {
+    "revision": "70501ebb3200f5e0eaaa86e010500f69",
+    "url": "/whether-app/static/media/heart.70501ebb.svg"
+  },
+  {
+    "revision": "58de3086d267f2f582930c027dffd60e",
+    "url": "/whether-app/static/media/search.58de3086.svg"
+  },
+  {
+    "revision": "c0c6d09293d93dae950739728dda611e",
+    "url": "/whether-app/static/media/spinner.c0c6d092.svg"
+  },
+  {
+    "revision": "ab9af4d0d33bc8a2b0d7cffe5be51758",
+    "url": "/whether-app/static/media/thermometer.ab9af4d0.svg"
+  }
+]);
